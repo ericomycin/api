3a5885e3ea98c46521800760911e2d3a8782d006
Opoku Eric <opokueric790@gmail.com>
2024-11-03 23:08:04 +0000

    dotenv

---
+++ main.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv, { configDotenv } from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT
+
+
+app.get("/", (req, res) =>{
+  const people = [
+    {
+      id: 1,
+      name: `Eric`,
+      age:24
+    },
+    {
+      id: 2,
+      name: `Opoku`,
+      age:54
+    },
+    {
+      id: 3,
+      name: `Beatrice`,
+      age:90
+    },
+    {
+      id: 4,
+      name: `Boahene`,
+      age:78
+    }
+  ]
+  res.send(people)
+})
+
+app.listen(port, () => {
+  console.log(`App runnung at ${port}`)
+})
